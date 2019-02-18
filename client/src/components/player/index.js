67f70c7d773a4104932dde985b62242901dd651b
Rogier Verkaik <rogierv@gmail.com>
2019-02-18 16:30:27 +0100

    Add Player Proptypes

--- client/src/components/player/index.js
+++ client/src/components/player/index.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import walkSprite from './player_walk.png';
 import handleMovement from './movement';
 
-function Player(props) {
+const Player = props => {
+  const { position, spriteLocation } = props;
+
   return (
     <div
       style={{
         position: 'absolute',
-        top: props.position[1],
-        left: props.position[0],
+        top: position[1],
+        left: position[0],
         backgroundImage: `url(${walkSprite})`,
-        backgroundPosition: props.spriteLocation,
+        backgroundPosition: spriteLocation,
         width: '40px',
         height: '40px'
       }}
     />
   );
-}
+};
+
+Player.propTypes = {
+  position: PropTypes.arrayOf(PropTypes.number).isRequired,
+  spriteLocation: PropTypes.string.isRequired
+};
 
 function mapStateToProps(state) {
   return {
