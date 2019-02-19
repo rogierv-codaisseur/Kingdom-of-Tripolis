4b0b31d64844d184ae044bdeee1b764805874808
Rogier Verkaik <rogierv@gmail.com>
2019-02-19 14:24:49 +0100

    Add second map

--- client/src/components/world/index.js
+++ client/src/components/world/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import Map from '../map';
 import Player from '../player/index';
-import Enemy from '../enemy/index'
+import Enemy from '../enemy/index';
 import Player2 from '../player2/index';
-import tiles from '../../data/maps/1';
+import level1 from '../../data/maps/1';
+import level2 from '../../data/maps/2';
 import store from '../../store';
 import { ADD_TILES } from '../../constants/actionTypes';
 
-const World = () => {
-  store.dispatch({
-    type: ADD_TILES,
-    payload: {
-      tiles
-    }
-  });
+const World = ({ match }) => {
+  const level = match.params.id;
+  switch (level) {
+    case '1':
+      store.dispatch({
+        type: ADD_TILES,
+        payload: {
+          tiles: level1
+        }
+      });
+      break;
+    case '2':
+      store.dispatch({
+        type: ADD_TILES,
+        payload: {
+          tiles: level2
+        }
+      });
+      break;
+    default:
+      break;
+  }
+
   return (
     <div
       style={{
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
@@ -8,7 +9,9 @@ import Store from './store';
 
 ReactDOM.render(
   <Provider store={Store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
