5dc36af76f54421c691b4722b749f345a7be1e9d
Rogier Verkaik <rogierv@gmail.com>
2019-02-22 10:10:12 +0100

    Add next level message

--- client/src/components/world/index.js
+++ client/src/components/world/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import Map from '../map';
 import Player from '../player/index';
 import Enemy from '../enemy/index';
